8521424313e3160fa670f14c52028e97738f5e70
DUYLANG <hungvumix@gmail.com>
2025-07-22 09:55:12 +0700

    frame layout

--- src/components/Layout/components/Sidebar/index.jsx
+++ src/components/Layout/components/Sidebar/index.jsx
@@ -1,5 +1,10 @@
+import styles from "./Sidebar.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return <div className={cx("wrapper")}>Sidebar</div>;
 };
 
 export default Sidebar;
